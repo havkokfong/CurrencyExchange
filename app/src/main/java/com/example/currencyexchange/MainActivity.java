33b40a5117f4e1c9a3792f0f81d6aacb2442aee0
Hav Kokfong <havkokfong@my.jcu.edu.au>
2019-04-15 19:04:51 +0800

    edit spinner reference

--- app/src/main/java/com/example/currencyexchange/MainActivity.java
+++ app/src/main/java/com/example/currencyexchange/MainActivity.java
@@ -58,10 +58,11 @@ public class MainActivity extends AppCompatActivity {
 
 
         Spinner spinner1 = findViewById(R.id.Spinner1);
-        Spinner spinner2 = findViewById(R.id.Spinner2);
+        final Spinner spinner2 = findViewById(R.id.Spinner2);
 
         mAdapter = new CountryAdapter(this, countryList);
         spinner1.setAdapter(mAdapter);
+        spinner2.setAdapter(mAdapter);
         spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -69,7 +70,17 @@ public class MainActivity extends AppCompatActivity {
                 String countrySelectedName = countrySelected.getCountryNameV();
                 Toast.makeText(MainActivity.this, countrySelectedName + " selected", Toast.LENGTH_SHORT).show();
                 jsonparse(countrySelectedName);
+                spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+                });
             }
 
             @Override
@@ -78,23 +89,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mAdapter = new CountryAdapter(this, countryList);
-        spinner2.setAdapter(mAdapter);
-        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                CountryList countrySelected = (CountryList) parent.getItemAtPosition(position);
-                String countrySelectedName = countrySelected.getCountryNameV();
-                Toast.makeText(MainActivity.this, countrySelectedName + " selected", Toast.LENGTH_SHORT).show();
-
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
 
     }
 

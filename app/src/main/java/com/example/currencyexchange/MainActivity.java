9486bda07c100450b712e14962196cf23047d871
De-ToNyS <havkokfong@my.jcu.edu.au>
2019-04-23 18:04:14 +0800

    added setting option

--- app/src/main/java/com/example/currencyexchange/MainActivity.java
+++ app/src/main/java/com/example/currencyexchange/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.currencyexchange;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,6 +20,7 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -28,6 +34,8 @@ import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+
+import static com.example.currencyexchange.R.id.action_setting;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.action_bar);
+        setSupportActionBar(toolbar);
 
         editText1 = findViewById(R.id.editText1);
         viewText2 = findViewById(R.id.viewText2);
@@ -463,4 +473,24 @@ public class MainActivity extends AppCompatActivity {
        editText1.setText(null);
        viewText2.setText(null);
    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case action_setting:
+                Intent setting = new Intent (this, setting_options.class);
+                startActivity(setting);
+                break;
+            default:
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
---
+++ app/src/main/java/com/example/currencyexchange/setting_options.java
@@ -0,0 +1,13 @@
+package com.example.currencyexchange;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class setting_options extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_options);
+    }
+}
